27f3019c63c6186bd225ec45a61dfc3895a97596
Avichai Alafi <Alafiwork@gmail.com>
2020-06-02 17:52:52 +0300

    first upload

---
+++ TestSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSwift
+//
+//  Created by  Avichai Alafi on 02/06/2020.
+//  Copyright © 2020 Avichai Alafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
